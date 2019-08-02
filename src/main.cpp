66d04aa6e6bce02767de8d70aa0c61dd8a2510da
Arkwolf <50710252+MajorArkwolf@users.noreply.github.com>
2019-08-02 15:39:34 +0800

    Add Mouse Movement

--- src/main.cpp
+++ src/main.cpp
@@ -423,8 +423,8 @@ int main(int argc, char **argv) {
     glutMouseFunc(Mouse);
 
     // ONLY USE IF REQUIRE MOUSE MOVEMENT
-    // glutPassiveMotionFunc(mouseMove);
-    // ShowCursor(FALSE);
+    glutPassiveMotionFunc(mouseMove);
+    while(ShowCursor(FALSE) != 0);
 
     glutReshapeFunc(reshape);
     glutMainLoop();
