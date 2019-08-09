c3714a4a8dcfd0f3b58b114e76e3488da866b46d
KieraG <KieraJGibson0@gmail.com>
2019-08-09 14:29:45 +0800

    Fix windows build

--- src/Gl.hpp
+++ src/Gl.hpp
@@ -4,6 +4,13 @@
 #    define GL_SILENCE_DEPRECATION
 #endif
 
+#if defined(_WIN32)
+#    include <windows.h>
+/* windows.h *helpfully* defines a macro for near and far. */
+#    undef near
+#    undef far
+#endif
+
 #if __has_include(<OpenGL/gl.h>)
 #    include <OpenGL/gl.h>
 #    include <OpenGL/glu.h>
@@ -13,11 +20,6 @@
 #endif
 
 #if defined(_WIN32)
-#    include <windows.h>
-/* windows.h *helpfully* defines a macro for near and far. */
-#    undef near
-#    undef far
-
 /* Windows GLUT doesn't have the normal BGR. */
 #    ifndef GL_BGR
 #        define GL_BGR GL_BGR_EXT
