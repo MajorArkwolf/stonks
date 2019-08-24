d24065d29d950ee4eae48f6f1d8edb3cf7de3606
Sandro Stikić <sandro@stikic.me>
2019-08-24 11:27:15 +0800

    Update download-github-asset.sh

--- .travis/download-github-asset.sh
+++ .travis/download-github-asset.sh
@@ -1,17 +1,37 @@
+#!/usr/bin/env bash
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+# Check dependencies.
+set -e
+type curl grep sed tr >&2
+xargs=$(which gxargs || which xargs)
+
+# Validate settings.
+[ -f ~/.secrets ] && source ~/.secrets
+[ "$GITHUB_TOKEN" ] || { echo "Error: Please define GITHUB_TOKEN variable." >&2; exit 1; }
+[ $# -ne 4 ] && { echo "Usage: $0 [owner] [repo] [tag] [name]"; exit 1; }
+[ "$TRACE" ] && set -x
 read owner repo tag name <<<$@
 
-GITHUB_API="https://api.github.com"
-GITHUB_REPO="$GITHUB_API/repos/$owner/$repo"
-GITHUB_TAGS="$GITHUB_REPO/releases/tags/$tag"
+# Define variables.
+GH_API="https://api.github.com"
+GH_REPO="$GH_API/repos/$owner/$repo"
+GH_TAGS="$GH_REPO/releases/tags/$tag"
 AUTH="Authorization: token $GITHUB_TOKEN"
 WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
 
+# Validate token.
+curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
+
 # Read asset tags.
-response=$(curl -sH "$AUTH" $GITHUB_TAGS)
+response=$(curl -sH "$AUTH" $GH_TAGS)
 # Get ID of the asset based on given name.
 eval $(echo "$response" | grep -C3 "name.:.\+$name" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-GITHUB_ASSET="$GITHUB_REPO/releases/assets/$id"
+[ "$id" ] || { echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2; exit 1; }
+GH_ASSET="$GH_REPO/releases/assets/$id"
 
-# Download asset.
-curl $CURL_ARGS -H 'Accept: application/octet-stream' "$GITHUB_ASSET?access_token=$GITHUB_TOKEN"
+# Download asset file.
+echo "Downloading asset..." >&2
+curl $CURL_ARGS -H 'Accept: application/octet-stream' "$GH_ASSET?access_token=$GITHUB_TOKEN"
+echo "$0 done." >&2
