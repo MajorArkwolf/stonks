e196a0973b0667e65eefaf2ece5be0381b7964ca
Peter <petercrabbe92@gmail.com>
2019-10-13 11:42:24 +0800

    Add random dice roller

---
+++ src/Akuma/Dice.cpp
@@ -0,0 +1,30 @@
+#include "Dice.hpp"
+
+#include <random>
+/**
+ * @brief  Rolls X amount of dice with X sides
+ *
+ * @param diceNumber Number of dice to roll
+ * @param diceSided Number of sides on the dice
+ * @return Returns running total of dice rolled
+ */
+auto Dice::Roll(const int diceNumber, const int diceSided) -> int {
+    auto runningTotal = int();
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(1, diceSided);
+    for (auto i = 0; i < diceNumber; ++i) {
+        runningTotal += dist(mt);
+    }
+    return runningTotal;
+}
+
+/**
+ * @brief  Rolls 1 dice with X sides
+ *
+ * @param diceSided Number of sides on the dice
+ * @return Returns running total of dice rolled
+ */
+auto Dice::Roll(const int diceSided) -> int {
+    return Roll(1, diceSided);
+}
