1871f912aa6efbf03ca3614aa902b225f98a7709
Peter Crabbe <major@DESKTOP-U36U4AB>
2019-10-19 01:46:41 +0800

    ModelManager draw calls now work.

--- src/ObjLoader/ModelManager.hpp
+++ src/ObjLoader/ModelManager.hpp
@@ -10,8 +10,10 @@ public:
     static auto GetModelID(std::string filename) -> size_t {
         static std::map<std::string, size_t> nameToId = {};
         auto id = nameToId.find(filename);
-        if (id != nameToId.end()) { // file not loaded yet
+        if (id == nameToId.end()) { // file not loaded yet
             ModelRepo().push_back(OBJ::Load(filename));
+            nameToId.emplace(filename, ModelRepo().size() - 1);
+            return ModelRepo().size() - 1;
         } else {
             return id->second;
         }
@@ -21,15 +23,16 @@ public:
                           const glm::vec3 &offset) {
         glPushMatrix();
         glTranslatef(offset.x, offset.y, offset.z);
-        // ModelDisplay::displayModel(ModelRepo()[id], scale, 1);
-        throw "DrawModel( ... ) not implemented";
+        OBJ::displayModel(ModelRepo().at(id), scale, 1);
+        //throw "DrawModel( ... ) not implemented";
         glPopMatrix();
     }
     static auto DrawModel(size_t id, const glm::vec3& offset) -> void {
         DrawModel(id, {1,1,1}, offset);
+        
     }
 private:
-    static auto ModelRepo() -> std::vector<Model> {
+    static auto ModelRepo() -> std::vector<Model>& {
         static std::vector<Model> m = {};
         return m;
     }
