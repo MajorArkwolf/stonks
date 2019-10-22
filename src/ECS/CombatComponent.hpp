42eeec84d141933cfa646bb1e105707c2fd060ed
Peter <petercrabbe92@gmail.com>
2019-10-22 16:34:53 +0800

    add hit to combat component

--- src/ECS/CombatComponent.hpp
+++ src/ECS/CombatComponent.hpp
@@ -1,26 +1,73 @@
 #pragma once
 
-#include "ECS.hpp"
 #include "../Akuma/Pathing/Node.hpp"
+#include "../Akuma/RNG/Dice.hpp"
+#include "ECS.hpp"
+#include "StatComponent.hpp"
+#include "TurnComponent.hpp"
 
-/* This will need to handle input to the player */
 class CombatComponent : public Component {
   public:
-    CombatComponent() = default;
+    CombatComponent()  = default;
     ~CombatComponent() = default;
     void init() {}
     void update() {}
     void draw() {}
 
-	void attackEntity(Entity *opponent) {
+    void attackEntity(Entity *opponent) {
+        // weaponCrit
+        // WeaponDamage
+        int diceRoll             = diceroller.Roll(1, 20);
+        int weaponHitDie         = 3;
+        int weaponPreHitDie      = 1;
+        int weaponCritMultiplier = 3;
+        int enemyAC              = 0;
+        int enemyDodge           = 0;
+        int weaponCritRange      = 0;
+        int enemyDodgePenalty    = 0;
+        int critrange            = 20 - weaponCritRange -
+                        this->entity->getComponent<StatComponent>().getLuckMod();
+        if (opponent->hasComponent<StatComponent>()) {
+            if (false /*opponent->hasComponent<EquipmentComponent>()*/) {
+                // enemyAC =
+                // enemyDodgePenalty
+            }
+            if (diceRoll == 1) {
+				//You fumbled
+            }else if (diceRoll == 20) {
+                diceRoll = diceroller.Roll(1, 20);
+                if (diceRoll >= ((enemyAC + enemyDodge) - enemyDodgePenalty)) {
+                    // crit damage
+                } else {
+                    // regular damage
+                }
+            } else if (diceRoll >= ((enemyAC + enemyDodge) - enemyDodgePenalty)) {
+                if (diceRoll >= critrange) {
+                    // damage multiplier
+                } else {
+                    // regular damage
+                }
+            }
+        }
+        this->entity->getComponent<TurnComponent>().endYourTurn();
+    }
 
-	}
+    int weaponDamage(int weaponHitDie, int weaponPreHitDie,
+                     int weaponCritMultiplier) {
+        int totalDamage = 0;
+        for (int i = 0; i < weaponPreHitDie; i++) {
+			
+		}
 
-	void attackEntity(Pathing::Node opponentSquare) {
+        return totalDamage;
+    }
 
-	}
+    void attackEntity(Pathing::Node *opponentSquare) {
+        attackEntity(opponentSquare->occupant);
+    }
 
   private:
+    Dice diceroller;
     Entity *opponent = nullptr;
-    bool turnToken                 = false;
+    bool turnToken   = false;
 };
