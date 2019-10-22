edaf950f209e8df77cf9d14d1adea52f61f171ac
Peter <petercrabbe92@gmail.com>
2019-10-22 22:13:00 +0800

    add more to loader

--- src/Akuma/Items/ItemManager.cpp
+++ src/Akuma/Items/ItemManager.cpp
@@ -1,9 +1,5 @@
 #include "ItemManager.hpp"
 
-
-
-
-
 void ItemLoader::init() {
     auto fileinput   = ifstream();
     auto currentLine = string();
@@ -22,22 +18,23 @@ void ItemLoader::init() {
 	}
 }
 
-void constructWeapon(string item) {
+void ItemLoader::constructWeapon(string item) {
     Weapon temp;
     stringstream ss = stringstream(item);
     ss >> temp;
-
 
 	temp.itemID = ItemManager::getNewID();
 	ItemID newID;
     newID.itemID = temp.itemID;
     newID.type   = weapon;
     newID.name   = temp.name;
+    newID.description   = temp.description;
     ItemManager::ItemManager().push_back(newID);
     ItemManager::WeaponManager().push_back(temp);
 
 }
-void constructArmor(string item) {
+
+void ItemLoader::constructArmor(string item) {
     Armor temp;
     stringstream ss = stringstream(item);
     ss >> temp;
@@ -48,10 +45,12 @@ void constructArmor(string item) {
     newID.itemID = temp.itemID;
     newID.type   = armor;
     newID.name   = temp.name;
+    newID.description = temp.description;
     ItemManager::ItemManager().push_back(newID);
     ItemManager::ArmorManager().push_back(temp);
 }
-void constructConsumable(string item) {
+
+void ItemLoader::constructConsumable(string item) {
     Consumable temp;
     stringstream ss = stringstream(item);
     ss >> temp;
@@ -62,6 +61,7 @@ void constructConsumable(string item) {
     newID.itemID = temp.itemID;
     newID.type   = consumable;
     newID.name   = temp.name;
+    newID.description = temp.description;
     ItemManager::ItemManager().push_back(newID);
     ItemManager::ConsumableManager().push_back(temp);
 }
--- src/Akuma/Items/ItemManager.hpp
+++ src/Akuma/Items/ItemManager.hpp
@@ -19,17 +19,43 @@ struct ItemID {
     size_t itemID = 0;
     itemType type = empty;
     string name   = "";
+    string description = "";
 };
 
 struct Armor {
     size_t itemID     = 0;
     itemType type     = armor;
     string name       = "";
+    string description = "";
     rarityItem rarity = common;
     int armorBonus    = 0;
     int dexPentalty   = 0;
-    istream &operator>>(std::istream &input) {
+    friend istream &operator>>(std::istream &input, Armor &item) {
+        string inputString              = "";
         static constexpr char SEPARATOR = ',';
+        std::getline(input, inputString, SEPARATOR);
+        item.name = inputString;
+
+        std::getline(input, inputString, SEPARATOR);
+        item.description = inputString;
+
+        std::getline(input, inputString, SEPARATOR);
+        if (inputString == "common") {
+            item.rarity = common;
+        } else if (inputString == "uncommon") {
+            item.rarity = uncommon;
+        } else if (inputString == "rare") {
+            item.rarity = rare;
+        } else if (inputString == "mythic") {
+            item.rarity = mythic;
+        }
+        std::getline(input, inputString, SEPARATOR);
+        item.armorBonus = std::stoi(inputString);
+
+        std::getline(input, inputString, SEPARATOR);
+        item.dexPentalty = std::stoi(inputString);
+
+        return input;
 	}
 };
 
@@ -37,14 +63,49 @@ struct Weapon {
     size_t itemID     = 0;
     itemType type     = weapon;
     string name       = "";
+    string description  = "";
     rarityItem rarity   = common;
     int weaponPreHitDie = 1;
     int weaponHitDie    = 3;
     int critRange       = 0;
     int critMultiplier  = 2;
-    istream &operator>>(std::istream &input) {
+    int range           = 1;
+    friend istream &operator>>(std::istream &input, Weapon &item) {
         string inputString              = "";
         static constexpr char SEPARATOR = ',';
+		std::getline(input, inputString, SEPARATOR);
+        item.name = inputString;
+
+		std::getline(input, inputString, SEPARATOR);
+        item.description = inputString;
+
+		std::getline(input, inputString, SEPARATOR);
+        if (inputString == "common") {
+            item.rarity = common;
+        } else if (inputString == "uncommon") {
+            item.rarity = uncommon;
+		} else if (inputString == "rare") {
+            item.rarity = rare;
+        } else if (inputString == "mythic") {
+            item.rarity = mythic;
+		}
+        std::getline(input, inputString, SEPARATOR);
+        item.weaponPreHitDie = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.weaponHitDie = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.critRange = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.critMultiplier = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.range = std::stoi(inputString);
+
+
+        return input;
     }
 };
 
@@ -52,6 +113,7 @@ struct Consumable {
     size_t itemID     = 0;
     itemType type     = consumable;
     string name       = "";
+    string description = "";
     rarityItem rarity = common;
     int HP            = 0;
     int strength      = 0;
@@ -60,8 +122,44 @@ struct Consumable {
     int intelligence  = 0;
     int vitality      = 0;
     int duration      = 0; //in seconds
-    istream &operator>>(std::istream &input) {
+    friend istream &operator>>(std::istream &input, Consumable &consumable1) {
+        string inputString              = "";
         static constexpr char SEPARATOR = ',';
+        std::getline(input, inputString, SEPARATOR);
+        item.name = inputString;
+
+        std::getline(input, inputString, SEPARATOR);
+        item.description = inputString;
+
+        std::getline(input, inputString, SEPARATOR);
+        if (inputString == "common") {
+            item.rarity = common;
+        } else if (inputString == "uncommon") {
+            item.rarity = uncommon;
+        } else if (inputString == "rare") {
+            item.rarity = rare;
+        } else if (inputString == "mythic") {
+            item.rarity = mythic;
+        }
+        std::getline(input, inputString, SEPARATOR);
+        item.HP = std::stoi(inputString);
+
+        std::getline(input, inputString, SEPARATOR);
+        item.strength = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.dexterity = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.luck = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.intelligence = std::stoi(inputString);
+
+		std::getline(input, inputString, SEPARATOR);
+        item.duration = std::stoi(inputString);
+
+        return input;
     }
 };
 
