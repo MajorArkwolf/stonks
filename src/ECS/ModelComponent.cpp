ba9b28d0e7ab39a0871d5356bd067e5db447e5eb
jammy-dodgers <derpy.l.llama@gmail.com>
2019-10-23 13:33:01 +0800

    Model component cpp

---
+++ src/ECS/ModelComponent.cpp
@@ -0,0 +1,36 @@
+#include "ModelComponent.hpp"
+
+void ModelComponent::init() {
+    this->modelId  = 0;
+    this->hasModel = false;
+    this->hasScale = this->entity->hasComponent<ScaleComponent>();
+    if (hasScale) {
+        this->scale = &this->entity->getComponent<ScaleComponent>();
+    }
+    this->hasPosition = this->entity->hasComponent<PositionComponent>();
+    if (hasPosition) {
+        this->pos = &this->entity->getComponent<PositionComponent>();
+    }
+}
+void ModelComponent::setModel(std::string filename) {
+    hasModel = true;
+    modelId  = ModelManager::GetModelID(filename);
+}
+void ModelComponent::update() {}
+void ModelComponent::draw() {
+    glm::vec3 entityScale{1, 1, 1};
+    glm::vec3 entityPos{0, 0, 0};
+    float entityRotation = 0;
+    if (hasModel) {
+        if (hasScale) {
+            entityScale = this->entity->getComponent<ScaleComponent>().getScale();
+        }
+        if (this->entity->hasComponent<PositionComponent>()) {
+            entityPos = this->entity->getComponent<PositionComponent>().getPos();
+            entityRotation =
+                this->entity->getComponent<PositionComponent>().getRotation();
+        }
+        ModelManager::DrawModel(this->modelId, entityRotation, entityScale,
+                                entityPos);
+    }
+}
--- src/ECS/ModelComponent.hpp
+++ src/ECS/ModelComponent.hpp
@@ -11,48 +11,16 @@ class ModelComponent : public Component {
     ModelComponent()  = default;
     ~ModelComponent() = default;
 
-    void init() {
-        this->modelId  = 0;
-        this->hasModel = false;
-        this->hasScale = this->entity->hasComponent<ScaleComponent>();
-        if (hasScale) {
-            this->scale = &this->entity->getComponent<ScaleComponent>();
-        }
-        this->hasPosition = this->entity->hasComponent<PositionComponent>();
-        if (hasPosition) {
-            this->pos = &this->entity->getComponent<PositionComponent>();
-        }
-    }
-    void setModel(std::string filename) {
-        hasModel = true;
-        modelId  = ModelManager::GetModelID(filename);
-    }
-    void update() {}
-    void draw() {
-        glm::vec3 entityScale{1, 1, 1};
-        glm::vec3 entityPos{0, 0, 0};
-        float entityRotation = 0;
-        if (hasModel) {
-            if (hasScale) {
-                entityScale =
-                    this->entity->getComponent<ScaleComponent>().getScale();
-            }
-            if (this->entity->hasComponent<PositionComponent>()) {
-                entityPos =
-                    this->entity->getComponent<PositionComponent>().getPos();
-                entityRotation =
-                    this->entity->getComponent<PositionComponent>().getRotation();
-            }
-            ModelManager::DrawModel(this->modelId, entityRotation, entityScale,
-                                    entityPos);
-        }
-    }
+    void init();
+    void setModel(std::string filename);
+    void update();
+    void draw();
 
   private:
-    size_t modelId = 0;
-    bool hasModel  = false;
-    bool hasScale  = false;
-    ScaleComponent *scale = nullptr;
-    bool hasPosition = false;
+    size_t modelId         = 0;
+    bool hasModel          = false;
+    bool hasScale          = false;
+    ScaleComponent *scale  = nullptr;
+    bool hasPosition       = false;
     PositionComponent *pos = nullptr;
 };
